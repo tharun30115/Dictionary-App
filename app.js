9c6d490961b2de6ded1978a030cdd4385e20ef97
Tharun <tharungowda30115@gmail.com>
2025-01-27 20:49:49 +0530

    js updation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//dictionary app fetch url --> https://api.dictionaryapi.dev/api/v2/entries/en/
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+
 
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
