c3890badd2e5e2cf77899ff263ae2ff570587d7f
Tharun <tharungowda30115@gmail.com>
2025-01-27 20:53:44 +0530

    minor updates

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
-
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
     fetch(`${url}${inpWord}`)
